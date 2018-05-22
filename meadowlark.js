54eac8544e528950feb282c47d38eb25bcedac1c
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2018-05-22 16:10:43 -0400

    set up mocha and jshint for qa

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var fortune = require('./lib/fortune.js')
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
@@ -10,13 +10,21 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next){
+  res.locals.showTests = app.get('env') !== 'production' &&
+      req.query.test === 1;
+  next();
+});
+// routes
 app.get('/', function(req, res){
   res.render('home');
 });
 app.get('/about', function(req,res){
-  res.render('about', {fortune: fortune.getFortune() } );
+  res.render('about', {fortune: fortune.getFortune(),
+                       pageTestScript: '/qa/tests-about.js'
+                     } );
 });
 
 //custom 404 page
@@ -35,8 +43,8 @@ app.use(function(err, req, res, next){
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost' +
-    app.get('port') + '; press ctrl-C to terminate')
-})
+    app.get('port') + '; press ctrl-C to terminate');
+});
 
 var fortunes = [
   "Conquer your fears or they will conquer you.", "Rivers need springs.",
