206a672fde1836a92c60f081c44584e63d3a1902
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2018-05-23 15:39:07 -0400

    create form to upload file

--- meadowlark.js
+++ meadowlark.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
+var formidable = require('formidable');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser')
 
 var app = express();
 
-var handlebars = require('express3-handlebars')
-  .create({defaultLayout: 'main'});
-app.engine('handlebars', handlebars.engine);
+var hbs = exphbs.create({
+  defaultLayout: 'main',
+  helpers: {
+    section: function(name, options){
+      if(!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+});
+
+app.engine('handlebars', hbs.engine);
+
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
@@ -17,6 +30,13 @@ app.use(function(req, res, next){
       req.query.test === 1;
   next();
 });
+
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
+
+
+
 // routes
 app.get('/', function(req, res){
   res.render('home');
@@ -25,6 +45,38 @@ app.get('/about', function(req,res){
   res.render('about', {fortune: fortune.getFortune(),
                        pageTestScript: '/qa/tests-about.js'
                      } );
+});
+
+app.get('/newsletter', function(req, res){
+  res.render('newsletter', {csrf: "CSRF toekn goes here"})
+});
+
+app.post('/process', function(req, res){
+  if(req.xhr || req.accepts('json,html')==='json'){
+    //if there were an error, we would send { error: 'error description' }
+    res.send({success: true});
+  } else {
+    res.redirect(303, '/thank-you');
+  }
+});
+
+app.get('/contest/vacation-photo', function(req,res){
+  var now = new Date();
+  res.render('contest/vacation-photo',{
+    year: now.getFullYear(), month: now.getMonth()
+  });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req,res){
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files){
+    if(err) return res.redirect(303, '/error')
+    console.log('received fields:');
+    console.log(fields);
+    console.log("received files:");
+    console.log(files);
+    res.redirect(303, '/thank-you')
+  });
 });
 
 //custom 404 page
@@ -45,9 +97,3 @@ app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost' +
     app.get('port') + '; press ctrl-C to terminate');
 });
-
-var fortunes = [
-  "Conquer your fears or they will conquer you.", "Rivers need springs.",
-"Do not fear what you don't know.",
-"You will have a pleasant surprise.", "Whenever possible, keep it simple.",
-];
