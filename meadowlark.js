dab826d9fbba79257c6ce6cc09a8225067f6f164
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2018-05-29 15:42:46 -0400

    create email template to send using nodemailer

--- meadowlark.js
+++ meadowlark.js
@@ -4,8 +4,19 @@ var formidable = require('formidable');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser')
 var jqupload = require('jquery-file-upload-middleware');
+var credentials = require('./credentials.js');
+var carValidation = require('./lib/cartValidation.js');
+var nodemailer = require('nodemailer');
 
 var app = express();
+
+var mailTransport = nodemailer.createTransport('SMTP', {
+    service: 'Gmail',
+    auth: {
+      user: credentials.gmail.user,
+      pass: credentials.gmail.password,
+    }
+});
 
 var hbs = exphbs.create({
   defaultLayout: 'main',
@@ -36,8 +47,20 @@ app.use(bodyParser.urlencoded());
 
 app.use(bodyParser.json());
 
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')());
 
+app.use(function(req, res, next){
+  // if there's a flash message, transfer
+  // it to the context, then clear it
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
+});
 
+app.use(require('./lib/requiresWaiver.js'));
+app.use(cartValidation.checkWaivers);
+app.use(cartValidation.chechGuestCounts);
 // routes
 app.get('/', function(req, res){
   res.render('home');
@@ -91,6 +114,49 @@ app.use('/upload', function(req, res, next){
     },
   })(req,res,next);
 });
+
+app.post('/newsletter', function(req, res){
+  var name = req.body.name || '', email = req.body.email || '';
+  //input validation
+  if(!email.match(VALID_EMAIL_REGEX)) {
+    if(req.xhr) return res.json({ error: 'Invalid name email address.'});
+    req.session.flash = {
+      type: 'danger',
+      intro: 'Validation error!',
+      message: 'The email address you entered was not valid.',
+    };
+    return res.redirect(303, '/newsletter/archive');
+  }
+
+  new NewsletterSignup({name: name, email: email}).save(function(err){
+    if(err) {
+      if(req.xhr) return res.json({error: 'Database error.'});
+      req.session.flash = {
+        type: 'danger',
+        intro: "Database error!",
+        message: 'There was a database error; please try again later',
+      }
+      return res.redirect(303, '/newsletter/archive');
+    }
+    if(req.xhr) return res.json({ success: true});
+    req.session.flash = {
+      type: 'success',
+      intro: 'Thank You!',
+      message: 'You have now been signed up for the newsletter.',
+    };
+    return res.redirect(303, '/newsletter/archive');
+  });
+});
+
+mailTransport.sendMail({
+    from: '"Meadowlark Travel" <info@meadowlarktravel.com>',
+    to: 'steven.aguilar@stu.bmcc.cuny.edu',
+    subject: 'Your Meadowlark Travel Tour',
+    text: 'Thank you for booking your trip with Meadowlark Travel.  ' +
+                      'We look forward to your visit!',
+    }, function(err){
+            if(err) console.error('Unable to send email: ' + error);
+  });
 
 //custom 404 page
 app.use(function(req, res){
